ac683098c1cfc905e683d58bb2bd7532f48238fc
Franky777shg <frengky.sihombing.777@gmail.com>
2026-06-30 15:29:56 +0700

    exercise nilai siswa

--- array/exercise.js
+++ array/exercise.js
@@ -1,10 +1,25 @@
-let nilaiSiswa = [60, 100, 50, 85, 95, 70]
+let nilaiSiswa = [60, 100, 50, 85, 95, 70, 82, 65]
 // Cari:
 // Total semua nilai
 // Rata rata nilai
 // Nilai tertinggi
 // Nilai terendah
 
-for (let i = 0; i < 6; i++) {
-    // isi code nya di sini
+// Total semua nilai
+let total = 0 // 460
+
+for (let i = 0; i < nilaiSiswa.length; i++) { // i = 6
+    total += nilaiSiswa[i]
 }
+
+// Rata rata nilai
+let rataRata = total / nilaiSiswa.length
+
+console.log(`Ini total nilai siswa ${total}`)
+console.log(`Ini rata rata nilai siswa ${rataRata}`)
+
+// Nilai tertinggi dan terendah
+let sorted = nilaiSiswa.sort((a, b) => a - b)
+// console.log(sorted)
+console.log(`Ini nilai tertinggi ${nilaiSiswa[nilaiSiswa.length - 1]}`)
+console.log(`Ini nilai terendah ${nilaiSiswa[0]}`)
