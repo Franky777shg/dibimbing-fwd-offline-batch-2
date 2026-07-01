8a6887abec7b749fcddc55e6d01a2b3fa9ba4cbc
Franky777shg <frengky.sihombing.777@gmail.com>
2026-07-01 11:04:10 +0700

    init exercise

---
+++ function/exercise.js
@@ -0,0 +1,19 @@
+let input = [
+  { name: "Budi", role: "admin", age: 30 },
+  { name: "Siti", role: "user", age: 20 },
+  { name: "Andi", role: "admin", age: 25 },
+  { name: "Rina", role: "user", age: 35 }
+]
+
+const analyzeUsers = (users) => {
+    // Write code here
+}
+
+let hasil = analyzeUsers(input)
+console.log(hasil)
+// {
+//   totalUsers: 4,
+//   admins: 2,
+//   users: 2,
+//   averageAge: 27.5
+// }
