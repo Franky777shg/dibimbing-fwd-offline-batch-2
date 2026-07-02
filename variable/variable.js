0f60d1de199669c4670ee4dd62908a60d27f4d9f
Franky777shg <frengky.sihombing.777@gmail.com>
2026-07-02 09:56:51 +0700

    materi variable

---
+++ variable/variable.js
@@ -0,0 +1,38 @@
+// SCOPE VARIABLE
+// function scopeExample() {
+//     let a = "Kevin"
+//     var b = "Nathan"
+//     console.log(a) // ini bisa
+// }
+
+// console.log(a) // ini tidak bisa
+
+// if (1 < 5) {
+//     var c = "Frengky"
+// }
+
+// console.log(c)
+
+// RE-DECLARE
+// var a = "Nathan"
+// var a = "Frengky"
+// console.log(a)
+
+// let a = "Mobil"
+// let a = "Motor"
+// console.log(a)
+
+// RE-ASSIGN
+// let a = "Leci"
+// a = "Melon"
+// console.log(a)
+
+// const b = 20
+// b = 21
+// console.log(b)
+
+console.log(a)
+var a = "Meja"
+
+console.log(b)
+let b = "Kursi"
