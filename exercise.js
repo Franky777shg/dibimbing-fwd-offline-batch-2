e7a5ade4e453a788509fd2feda5464e5ad45a069
Franky777shg <frengky.sihombing.777@gmail.com>
2026-07-06 14:34:11 +0700

    answer exercise

--- exercise.js
+++ exercise.js
@@ -7,10 +7,23 @@ const products = [
   { name: "Table", category: "Furniture", price: 200 }
 ];
 
-// Tulis function nya di sini
 const findCheapestProduct = (arr, category) => {
     // Tulis code di sini
+
+    // 1. kumpulkan dulu data product yang sesuai category
+    let filteredProducts = arr.filter(item => item.category === category)
+    // console.log(filteredProducts)
+
+    // 2. cari product dengan price termurah diantara product yang sudah kita kumpulkan di atas
+    let sortedProducts = filteredProducts.sort((a, b) => a.price - b.price)
+    // console.log(sortedProducts)
+
+    // 3. setelah diurutkan, PASTI item termurah, itu ada di index ke 0
+    console.log(sortedProducts[0])
+    // return sortedProducts[0]
 }
 
 // Contoh
+// Function findCheapestProduct dipanggil
 findCheapestProduct(products, "Electronics");
+// console.log(findCheapestProduct(products, "Electronics"))
