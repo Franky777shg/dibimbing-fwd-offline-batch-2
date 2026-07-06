b806c7060db95902565f945ec75e141b29a13aba
Franky777shg <frengky.sihombing.777@gmail.com>
2026-07-06 13:12:07 +0700

    exercise.js

---
+++ exercise.js
@@ -0,0 +1,16 @@
+// Input
+const products = [
+  { name: "Laptop", category: "Electronics", price: 1200 },
+  { name: "Mouse", category: "Electronics", price: 25 },
+  { name: "Keyboard", category: "Electronics", price: 50 },
+  { name: "Chair", category: "Furniture", price: 100 },
+  { name: "Table", category: "Furniture", price: 200 }
+];
+
+// Tulis function nya di sini
+const findCheapestProduct = (arr, category) => {
+    // Tulis code di sini
+}
+
+// Contoh
+findCheapestProduct(products, "Electronics");
