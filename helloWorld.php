1c0ff31dfe7b2eaa85897f955ce0b573d578d7e4
Franky777shg <frengky.sihombing.777@gmail.com>
2026-08-14 13:54:00 +0700

    latihan deployment

---
+++ helloWorld.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Hello World!\n";
+echo "Hello ", "World!\n";
+echo "Umur: " . 25 . " Tahun\n";
+print "Hello World!";
+
+# Ini komentar memakai "#"
--- init-express/routes/cookie.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const cookieController = require('../controllers/cookie.controller')
-
-router.post('/set', cookieController.setCookie)
-router.get('/get', cookieController.getCookie)
-
-// untuk mengexport router di atas yang sudah dibuat
-module.exports = router
